d90c6e2c94e251a96fe80741bf0b2147ceb021a3
Mahdi <Mahdiporkar@gmail.com>
2025-12-29 23:28:05 +0330

    cors

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -19,6 +19,11 @@ function buildFieldErrors(errors: ValidationError[]) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
